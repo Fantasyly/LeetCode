18d9a78d644736e591c65ad4a936f6a0d2a746b7
young <mr_isliuy@163.com>
2020-11-17 12:33:41 +0800

    Add Python Solution for No.102

---
+++ 102/levelOrder.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+
+        queue, res = [], []
+        queue.append(root)
+
+        while queue:
+            size = len(queue)
+            level = []
+            for _ in range(size):
+                node = queue.pop(0)
+                level.append(node.val)
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
+
+            res.append(level)
+        return res
