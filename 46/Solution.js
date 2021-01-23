fc6408b5a10672111ae993268f728feeac823b86
young <mr_isliuy@163.com>
2021-01-23 19:19:02 +0800

    46题

---
+++ 46/Solution.js
@@ -0,0 +1,32 @@
+/**
+ * 回溯解决
+ * 时间复杂度： O()
+ * 空间复杂度
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+  if (!nums.length) return [];
+
+  // 记录当前位置的值是否被用过了
+  let used = new Array(nums.length).fill(false);
+  let res = [];
+
+  const dfs = path => {
+    if (path.length === nums.length) {
+      res.push([...path]);
+      return;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      if (used[i]) continue;
+      path.push(nums[i]);
+      used[i] = true;
+      dfs(path);
+      path.pop(nums[i]);
+      used[i] = false;
+    }
+  };
+  dfs([]);
+  return res;
+};
